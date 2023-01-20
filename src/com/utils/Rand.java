346f4fce52cc1088ee7aa9b89bed6efdcdcf4114
kot0fey <prutsk@mail.ru>
2023-01-20 16:30:35 +0300

    Fix request #6 from (18.01.2023) + Fixed bug with saving HP after restart + Found bug with one HP for two identical characters

--- src/com/utils/Rand.java
+++ src/com/utils/Rand.java
@@ -3,9 +3,6 @@ import java.util.Random;
 public class Rand {
     private static final Random rand = new Random();
 
-    public static int Int(){
-        return rand.nextInt();
-    }
     public static int Int(int min, int max){
         return rand.nextInt(min, max);
     }
